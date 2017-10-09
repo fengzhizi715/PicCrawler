744eecd02cf5c50d7442fce476be6038e6edff33
fengzhizi715 <fengzhizi715@126.com>
2017-10-09 23:06:54 +0800

    init the project

---
+++ app/src/main/java/com/cv4j/piccrawler/HttpClientWithPool.java
@@ -0,0 +1,176 @@
+package com.cv4j.piccrawler;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.ParseException;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.config.AuthSchemes;
+import org.apache.http.client.config.CookieSpecs;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.config.Registry;
+import org.apache.http.config.RegistryBuilder;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.socket.PlainConnectionSocketFactory;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.util.EntityUtils;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import java.io.*;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+import java.util.Arrays;
+
+
+/**
+ * Created by tony on 2017/9/11.
+ */
+public class HttpClientWithPool {
+
+    /** 全局连接池对象 */
+    private static PoolingHttpClientConnectionManager connManager = null;
+
+    /**
+     * 静态代码块配置连接池信息
+     */
+    static {
+        SSLContext sslcontext = null;
+        try {
+            //获取TLS安全协议上下文
+            sslcontext = SSLContext.getInstance("TLS");
+            sslcontext.init(null, new TrustManager[]{new X509TrustManager() {
+                @Override
+                public void checkClientTrusted(X509Certificate[] arg0, String arg1)
+                        throws CertificateException {
+                }
+                @Override
+                public void checkServerTrusted(X509Certificate[] arg0, String arg1)
+                        throws CertificateException {
+                }
+                @Override
+                public X509Certificate[] getAcceptedIssuers() {
+                    return new X509Certificate[]{};
+                }}}, null);
+            SSLConnectionSocketFactory scsf = new SSLConnectionSocketFactory(sslcontext, NoopHostnameVerifier.INSTANCE);
+            RequestConfig defaultConfig = RequestConfig.custom().setCookieSpec(CookieSpecs.STANDARD_STRICT)
+                    .setExpectContinueEnabled(true)
+                    .setTargetPreferredAuthSchemes(Arrays.asList(AuthSchemes.NTLM, AuthSchemes.DIGEST))
+                    .setProxyPreferredAuthSchemes(Arrays.asList(AuthSchemes.BASIC)).build();
+            Registry<ConnectionSocketFactory> sfr = RegistryBuilder.<ConnectionSocketFactory>create()
+                    .register("http", PlainConnectionSocketFactory.INSTANCE)
+                    .register("https", scsf).build();
+
+            connManager = new PoolingHttpClientConnectionManager(sfr);
+
+            // 设置最大连接数
+            connManager.setMaxTotal(200);
+            // 设置每个连接的路由数
+            connManager.setDefaultMaxPerRoute(20);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (KeyManagementException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * 获取Http客户端连接对象
+     *
+     * @param timeOut 超时时间
+     * @return Http客户端连接对象
+     */
+    public static CloseableHttpClient getHttpClient(int timeOut) {
+        // 创建Http请求配置参数
+        RequestConfig requestConfig = RequestConfig.custom()
+                // 获取连接超时时间
+                .setConnectionRequestTimeout(timeOut)
+                // 请求超时时间
+                .setConnectTimeout(timeOut)
+                // 响应超时时间
+                .setSocketTimeout(timeOut)
+                .build();
+
+        // 创建httpClient
+        return HttpClients.custom()
+                // 把请求相关的超时信息设置到连接客户端
+                .setDefaultRequestConfig(requestConfig)
+                // 把请求重试设置到连接客户端
+                .setRetryHandler(new RetryHandler())
+                // 配置连接池管理对象
+                .setConnectionManager(connManager)
+                .build();
+    }
+
+    /**
+     * Post请求
+     *
+     * @param url 请求地址
+     * @param timeOut 超时时间
+     * @return
+     */
+    public static String downloadPic(String url, int timeOut,int index) {
+        String msg = null;
+
+        // 获取客户端连接对象
+        CloseableHttpClient httpClient = getHttpClient(timeOut);
+        // 创建GET请求对象
+        HttpPost httpPost = new HttpPost(url);
+
+        CloseableHttpResponse response = null;
+
+        try {
+            // 执行请求
+            response = httpClient.execute(httpPost);
+            // 获取响应实体
+            HttpEntity entity = response.getEntity();
+
+            InputStream is = entity.getContent();
+            // 包装成高效流
+            BufferedInputStream bis = new BufferedInputStream(is);
+
+            // 写入本地
+            File file = new File("images/"+index+".jpg");
+            BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(file));
+
+            byte[] byt = new byte[1024 * 8];
+            Integer len = -1;
+            while ((len = bis.read(byt)) != -1) {
+                bos.write(byt, 0, len);
+            }
+
+            bos.close();
+            bis.close();
+
+        } catch (ClientProtocolException e) {
+            System.err.println("协议错误");
+            e.printStackTrace();
+        } catch (ParseException e) {
+            System.err.println("解析错误");
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.err.println("IO错误");
+            e.printStackTrace();
+        } finally {
+            if (response!= null) {
+                try {
+                    EntityUtils.consume(response.getEntity());
+                    response.close();
+                } catch (IOException e) {
+                    System.err.println("释放链接错误");
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return msg;
+    }
+}
